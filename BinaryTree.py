54598e201755292a93e0b7cc2e7aad151b675d29
Becky Maksimovic <beckymaksimovic@dhcp-mcfad-2556.redrover.cornell.edu>
2018-05-15 22:02:32 -0400

    this still has issues such as it seems to want to print a tree at every time step but otherwise seems mostly fine. I'm sure there are other problems

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,11 +4,6 @@
 #     def __init__(self, value):
 #       self.value = value
 
-_SPACE_PER_LEVEL = 10
-_EMPTY_LINE = ""
-for i in range(0, _SPACE_PER_LEVEL):
-    _EMPTY_LINE += ' '
-
 
 class BinaryTree():
     def __init__(self, root_value, left_value = None, right_value = None):
@@ -61,58 +56,14 @@ class BinaryTree():
             self.left.ageTree()
         if self.right:
             self.right.ageTree()
-            
-    def getDepth(self):
-        """Return depth of tree: log(number of nodes)"""
-        #TODO: Write this function
-        return 0
-
-    def printDict(self, d):
-        for k in d.keys():
-            print(str(k))
-
-    def printTree(self, output_file = "output.txt"):
-        """Print the tree into text file"""
-        depth = self.getDepth()
-        #Lines occupied by tree in file
-        num_lines = (2**(depth+1)) - 1        
-        print("Number of lines is {}.".format(num_lines))
-        line_data = dict()
-        self._get_line_data(depth, (num_lines//2)+1, line_data)
-        self.printDict(line_data)
-        with open(output_file, 'w') as f:
-            for line_no in range(1, num_lines+1):
-                print("Working on line {}.".format(line_no))
-                #Build string for this line
-                line = ""
-                if line_no in line_data:
-                    print("Line {} has something".format(line_no))
-                    line_dict = line_data[line_no]
-                    self.printDict(line_dict)
-                    for ht in range (0, depth+1):
-                        if ht in line_dict:
-                            bact = line_dict[ht]
-                            print("found bacteria {}".format(bact.name))
-                            line_length = _SPACE_PER_LEVEL - len(bact.name)
-                            horizontal_line = ""
-                            if ht!= 0:
-                            #Build a line of approrpiate length for the diagram
-                                for j in range(0, line_length):
-                                    horizontal_line+="-"
-                                horizontal_line+="|"
-                            line = bact.name + horizontal_line + line
-                        else:
-                            line = _EMPTY_LINE + line    
-                f.write(line)
-
     def _get_line_data(self, current_ht, mid_pt, d):
         """
         NOTE: CURRENT_HEIGHT = MAX_DEPTH - CURRENT_DEPTH so leaf will have deoth 0.
         Update dictionary d of form:
         {
         line no: {
-                  depth level : bacteria info
-                 }
+                    depth level : bacteria info
+                    }
         }
         For example fot the tree
             __b
@@ -120,9 +71,9 @@ class BinaryTree():
             --c 
         this is something like
         {
-          1: { 0 : Bacteria b }
-          2: { 1 : Bacteria a }
-          3: { 0 : Bacteria c }
+            1: { 0 : Bacteria b }
+            2: { 1 : Bacteria a }
+            3: { 0 : Bacteria c }
         }
         """
         #add self to midpoint
@@ -132,53 +83,7 @@ class BinaryTree():
         if self.left:
             self.left._get_line_data(current_ht, mid_pt - 2**current_ht)
         if self.right:
-            self.right._get_line_data(current_ht, midt_pt + 2**current_ht)
-            
-                
-    # def timeStep(self, cur_node): #step where the cell does not divide
-    # def divStep(self, cur_node): #step where the cell does divide
-    
-#     def findPath(root, path, k):
-#     # Base Case
-#         if root is None:
-#             return False
- 
-#     # Store this node is path vector. The node will be
-#     # removed if not in path from root to k
-#         path.append(root.key)
- 
-#     # See if the k is same as root's key
-#         if root.key == k :
-#             return True
- 
-#     # Check if k is found in left or right sub-tree
-#         if ((root.left != None and findPath(root.left, path, k)) or
-#             (root.right!= None and findPath(root.right, path, k))):
-#             return True
- 
-#     # If not present in subtree rooted with root, remove
-#     # root from path and return False
-
-#         path.pop()
-#         return False
- 
-# # Returns LCA if node n1 , n2 are present in the given
-# # binary tre otherwise return -1
-# def findLCA(root, n1, n2):
- 
-#     # To store paths to n1 and n2 fromthe root
-#     path1 = []
-#     path2 = []
- 
-#     # Find paths from root to n1 and root to n2.
-#     # If either n1 or n2 is not present , return -1 
-#         if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
-#             return -1
- 
-#     # Compare the paths to get the first different value
-#         i = 0
-#         while(i < len(path1) and i < len(path2)):
-#             if path1[i] != path2[i]:
-#                 break
-#             i += 1
-#         return path1[i-1]
+            self.right._get_line_data(current_ht, mid_pt + 2**current_ht)
+    def printDict(self, d):
+        for k in d.keys():
+            print(str(k))
--- Main.py
+++ Main.py
@@ -7,12 +7,18 @@ _LENGTH = 1
 _POS_X = 0
 _POS_Y = 0
 _AGE = 0
+_SPACE_PER_LEVEL = 10
+_EMPTY_LINE = ""
+for i in range(0, _SPACE_PER_LEVEL):
+    _EMPTY_LINE += ' '
+
 
 _DEBUG = True
 
 _TIME = 3 #Units of time that simulation runs
 _REPRODUCE_PROBABILITY = 0.5 #Probablility that bacteria reproduces given that it is long enough
 _MIN_LEN = 8 #Minimum length to be able to reproduce
+depth = (2*_TIME)+1
 
 _NAME_LIST = ["Jane", "Lucy", "Mark", "Marie", "Sarah", "Logan"]
 
@@ -38,6 +44,47 @@ def shouldDivide(bacteria):
         return True
     return False
 
+    
+
+
+
+
+def printTree(self, output_file = "output.txt"):
+   """Print the tree into text file"""
+   depth
+   #Lines occupied by tree in file
+   num_lines = (2**(depth+1)) - 1        
+   print("Number of lines is {}.".format(num_lines))
+   line_data = dict()
+   self._get_line_data(depth, (num_lines//2)+1, line_data)
+   self.printDict(line_data)
+   with open(output_file, 'w') as f:
+        for line_no in range(1, num_lines+1):
+            print("Working on line {}.".format(line_no))
+            #Build string for this line
+            line = ""
+            if line_no in line_data:
+                print("Line {} has something".format(line_no))
+                line_dict = line_data[line_no]
+                self.printDict(line_dict)
+                for ht in range (0, depth+1):
+                    if ht in line_dict:
+                        bact = line_dict[ht]
+                        print("found bacteria {}".format(bact.name))
+                        line_length = _SPACE_PER_LEVEL - len(bact.name)
+                        horizontal_line = ""
+                        if ht!= 0:
+                        #Build a line of approrpiate length for the diagram
+                            for j in range(0, line_length):
+                                horizontal_line+="-"
+                            horizontal_line+="|"
+                        line = bact.name + horizontal_line + line
+                    else:
+                        line = _EMPTY_LINE + line    
+            f.write(line)
+
+
+            
 if __name__ == '__main__':
     timothy = Bacteria(_LENGTH, _POS_X, _POS_Y, _AGE, "timothy")
     tree = BinaryTree(timothy)
@@ -53,10 +100,6 @@ if __name__ == '__main__':
                 print("Age: {}".format(str(child.root.age)))
                 print("Length: {}".format(str(child.root.len)))
         tree.ageTree()
-    tree.printTree()
-    _TIM = Bacteria(_LENGTH, _POS_X, _POS_Y, _AGE, "timothy")
-    _TIM_TREE = BinaryTree(_TIM) #Tree with a lonely Timothy as root
-    _TIM_TREE.printTree()
-
+        print(tree.root)
 
         
